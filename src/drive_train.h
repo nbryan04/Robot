265b9c5dd3dc896fced17d58dd37272da0617e65
Nathaniel <nbryan01@proton.me>
2026-07-07 16:57:14 -0700

    minor changes

--- src/drive_train.h
+++ src/drive_train.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "motor.h"
 struct drive_train {
-  drive_train(const Motor &motor1, const Motor &motor2);
+  drive_train(Motor motor1, Motor motor2);
 
   void turn(float angle); // int method?
   void drive(float distance, float speed); //set a default speed
