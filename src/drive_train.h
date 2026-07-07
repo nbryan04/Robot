ad87f53692737cc6a0dcba8d0bbc29161835f659
Nathaniel <nbryan01@proton.me>
2026-07-07 16:51:24 -0700

    added basic drive_train structures / methods

--- src/drive_train.h
+++ src/drive_train.h
@@ -1,1 +1,9 @@
+#pragma once
+#include "motor.h"
+struct drive_train {
+  drive_train(const Motor &motor1, const Motor &motor2);
 
+  void turn(float angle); // int method?
+  void drive(float distance, float speed); //set a default speed
+  void line_follow(float distance, float speed);
+}
