dbd688dd4da36af2a3e97a3ddb698eb1a3569035
Ben Rutherford-Braun <ben.rutherfordbraun@gmail.com>
2026-07-08 10:59:46 -0700

    Added line following constants

--- src/config.h
+++ src/config.h
@@ -14,5 +14,9 @@ inline constexpr float WHEEL_1_DIAMETER = 79; //(mm)
 inline constexpr float WHEEL_1_CIRCUMFERENCE = PI*WHEEL_1_DIAMETER;
 inline constexpr float WHEEL_1_SINGLE_PULSE = WHEEL_1_CIRCUMFERENCE / 1441; //(mm)
 
+//Line Following Constants
+inline constexpr float LF_KD = 0.95; // derivative coefficient
+inline constexpr float LF_KP = 0.8; // proportional coefficient
+inline constexpr int LF_THRESHOLD = 1500; // tape sensing
 
 }
