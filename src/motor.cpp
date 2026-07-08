5aa242b45905706804b6aa5e5b1e76c6a16f6bf3
JChin314 <jchin314159@gmail.com>
2026-07-08 16:03:48 -0700

    changed interupt logic for motor

--- src/motor.cpp
+++ src/motor.cpp
@@ -3,13 +3,6 @@
 #include "esp32-hal-ledc.h"
 #include <Arduino.h>
 
-//temporary function 
-int num;
-void count() {
-    num += 1;
-}
-
-
 Motor::Motor(int p1, int p2, int ePin1, int ePin2) {
     PWM_pin1 = p1;
     PWM_pin2 = p2;
@@ -20,8 +13,16 @@ Motor::Motor(int p1, int p2, int ePin1, int ePin2) {
 void Motor::begin() {
     ledcAttach(PWM_pin1, robotConfig::DRIVING_FREQUENCY, robotConfig::PWM_RESOLUTION);
     ledcAttach(PWM_pin2,robotConfig::DRIVING_FREQUENCY, robotConfig::PWM_RESOLUTION);
-    attachInterrupt(digitalPinToInterrupt(encoderPin1),count, RISING);
+    attachInterruptArg(digitalPinToInterrupt(encoderPin1), isrWrapper, this, RISING);
     //attachInterrupt(encoderPin2);
+}
+
+void IRAM_ATTR Motor::isrWrapper(void* arg) {
+    // 1. The hardware hands us a generic 'void*' memory address (arg).
+    // 2. We use static_cast to tell the compiler: "Trust me, this is actually a Motor object."
+    Motor* instance = static_cast<Motor*>(arg); 
+    // 3. Now that it knows it's a Motor, we can increment its specific counter!
+    instance->encoderCount += 1; 
 }
 
 void Motor::drive(float speed) {
@@ -31,13 +32,14 @@ void Motor::drive(float speed) {
 void Motor::drive_distance(float distance, float speed) {
 }
 
-//temporary method
+
+
 void Motor::one_turn(void) {
-    num = 0;
-    while(num <= 1441) {
-	ledcWrite(PWM_pin1, 400);
+    encoderCount = 0;
+    while(encoderCount <= 1441) {
+	    ledcWrite(PWM_pin1, 400);
     }
 	ledcWrite(PWM_pin1, 0);
-    }
+}
 
 
--- src/motor.h
+++ src/motor.h
@@ -9,10 +9,18 @@ public:
     bool isForward = true;
     float distance = 0.0f;
     float rotations = 0.0f;
+    
+    volatile long encoderCount = 0; 
+    unsigned long lastTime = 0;
+    long lastCount = 0;
+    float lastKnownSpeed = 0.0; 
+    
     Motor(int input_pin1, int input_pin2, int inputEncoderPin1, int inputEncoderPin2);
    //negative speed = backwards 
    void drive(float speed);
    void drive_distance(float distance, float speed = 1.0f);
    void one_turn(void);
    void begin();
+   float get_velocity();
+   static void isrWrapper(void* arg);
 };
